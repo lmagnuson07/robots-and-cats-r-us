121f039fb4a321e42a18a5045066e2ba1dfe1d6e
lmagnuson07 <logmagns07@gmail.com>
2022-11-05 12:24:23 -0600

    Added responsive styling

--- components/HeaderComponent.jsx
+++ components/HeaderComponent.jsx
@@ -1,18 +1,20 @@
 
 function HeaderComponent({tagline, title, imgSrc, imgAlt, ...props}) {
     return ( 
-        <header className="h-screen grid grid-rows-[120px_minmax(400px,_1fr)_120px] bg-gradient-to-br from-slate-500 via-gray-900 to-slate-900">
-            <img className="w-80 justify-self-center"
-                src={imgSrc} 
-                alt={imgAlt} 
-            />
-            <div className="self-center justify-self-center">
-                <h1 className="w-[fit-content] text-7xl font-serif text-stone-100 text-shadow mb-12 mx-auto border-black">
-                    {title}
-                </h1>
-                <p className="w-[fit-content] max-w-4xl text-xl text-center text-zinc-200">
-                    {tagline}
-                </p>
+        <header className="h-screen bg-gradient-to-br from-slate-500 via-gray-900 to-slate-900">
+            <div className="h-screen container grid grid-rows-[120px_minmax(400px,_1fr)_120px] justify-items-center mx-auto">
+                <img className="w-80 px-3"
+                    src={imgSrc} 
+                    alt={imgAlt} 
+                />
+                <div className="self-center mx-3">
+                    <h1 className="w-[fit-content] mb-12 mx-auto sm:text-7xl text-5xl text-center font-serif font-bold text-stone-100 text-shadow">
+                        {title}
+                    </h1>
+                    <p className="w-[fit-content] max-w-4xl sm:text-xl text-base text-center text-zinc-200">
+                        {tagline}
+                    </p>
+                </div>
             </div>
         </header>
     );
