58e2d241d7f8824d239d8d55da4fb82666803729
lmagnuson07 <logmagns07@gmail.com>
2022-11-10 15:01:44 -0700

    Converted logn png to webp format

--- pages/index.js
+++ pages/index.js
@@ -4,14 +4,13 @@ import { HeaderComponent } from '../components'
 export default function Home() {
   return (
     <>
-
       <Head>
         <title>Cats and Robots 'R Us</title>
       </Head>
       <HeaderComponent 
         title="Robots and Cats 'R Us"
         tagline="Purchase a robot for any of your personal needs or select from our large range of virtual cats, all of which will accompany you in the metaverse!"
-        imgSrc="img/logo.png"
+        imgSrc="img/logo.webp"
         imgAlt="Cats and Robots 'R Us logo"
         imgWidth="640"
         imgHeight="240"
