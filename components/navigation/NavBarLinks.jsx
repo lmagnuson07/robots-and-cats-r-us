045fc1a49162896654304045559a15812fa7d7a0
lmagnuson07 <logmagns07@gmail.com>
2022-12-03 19:32:57 -0700

    Fixed a google lighthouse error with my navigation link sizes. Removed 100 lines of test data.

--- components/navigation/NavBarLinks.jsx
+++ components/navigation/NavBarLinks.jsx
@@ -2,21 +2,21 @@ import Link from 'next/link'
 
 function NavBarLinks({customStyles, ...props}) {
     return ( 
-        <ul className={`${customStyles} gap-4`}>
-            <li>
-                <Link href="/">home</Link>
+        <ul className={`${customStyles} lg:gap-4`}>
+            <li className="w-full lg:w-[unset] text-center">
+                <Link className="w-full inline-block py-[0.25rem] lg:w-[unset] lg:py-[unset]" href="/">home</Link>
             </li>
-            <li>
-                <Link href="/service-robots">Service Robots</Link>
+            <li className="w-full lg:w-[unset] text-center">
+                <Link className="w-full inline-block py-[0.5rem] lg:w-[unset] lg:py-[unset]" href="/service-robots">Service Robots</Link>
             </li>
-            <li>
-                <Link href="/combat-robots">Combat Robots</Link> 
+            <li className="w-full lg:w-[unset] text-center">
+                <Link className="w-full inline-block py-[0.5rem] lg:w-[unset] lg:py-[unset]" href="/combat-robots">Combat Robots</Link> 
             </li>
-            <li>
-                <Link href="/social-robots">Social Robots</Link> 
+            <li className="w-full lg:w-[unset] text-center">
+                <Link className="w-full inline-block py-[0.5rem] lg:w-[unset] lg:py-[unset]" href="/social-robots">Social Robots</Link> 
             </li>
-            <li>
-                <Link href="/cats">Cats</Link> 
+            <li className="w-full lg:w-[unset] text-center">
+                <Link className="w-full inline-block py-[0.5rem] lg:w-[unset] lg:py-[unset]" href="/cats">Cats</Link> 
             </li>
         </ul>
      );
