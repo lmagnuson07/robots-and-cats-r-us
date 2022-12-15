fd711a9e559928e7bd2941d1ccdd62270914e762
lmagnuson07 <logmagns07@gmail.com>
2022-12-14 18:22:27 -0700

    Toggle navigation implemented

--- components/navigation/NavBar.jsx
+++ components/navigation/NavBar.jsx
@@ -1,23 +1,43 @@
 import LoginButton from './LoginButton';
 import GithubButton from './GithubButton';
-import MobileMenuButton from './MobileMenuButton';
 import NavBarBranding from './NavBarBranding';
 import NavBarLinks from './NavBarLinks';
-
+import { useState } from 'react'
 
 function NavBar() {
+    const [toggle, setToggle] = useState(false);
+    function handleClick(evt){
+        setToggle(!toggle);
+    }
     return ( 
         <header className="bg-gray-50 shadow-lg text-slate-600 font-bold fixed w-full px-3 py-4 z-20">
             <div className="container grid grid-cols-2 lg:grid-cols-[1fr,auto,1fr] justify-between relative items-center mx-auto">
                 <NavBarBranding />
-                <nav id="main-nav" className="col-[1/-1] lg:col-[2/3] fixed left-0 top-[72px] w-full bg-gray-50 lg:static opacity-0 max-h-0 lg:opacity-100 lg:max-h-12">
+                <nav id="main-nav" className={`col-[1/-1] lg:col-[2/3] fixed left-0 top-[72px] w-full bg-gray-50 lg:static ${toggle ? "opacity-100 max-h-[1000px]" : "opacity-0 max-h-0"} lg:opacity-100 lg:max-h-12`}>
                     <NavBarLinks customStyles="flex flex-col items-center lg:flex-row" />
                 </nav>
-                <div id="links" className="flex justify-around col-[1/-1] lg:col-[3/-1] fixed top-[252px] bg-gray-50 right-0 left-0 gap-4 m-auto w-full px-10 md:px-24 lg:justify-end py-4 lg:static lg:px-0 lg:py-0 opacity-0 lg:opacity-100 lg:max-h-12">
+                <div id="links" className={`flex justify-around col-[1/-1] lg:col-[3/-1] fixed top-[252px] bg-gray-50 right-0 left-0 gap-4 m-auto w-full px-10 md:px-24 lg:justify-end py-4 lg:static lg:px-0 lg:py-0 ${toggle ? "opacity-100 max-h-24" : "opacity-0 max-h-0"} lg:opacity-100 lg:max-h-12`}>
                     <GithubButton className="flex" />
                     <LoginButton className="flex" />
                 </div>
-                <MobileMenuButton id="nav-button" className="col-[2/3] row-[1/-1] justify-self-end lg:hidden"/> 
+                <button
+                    title="Toggle Button"
+                    type="button"
+                    className= {`col-[2/3] row-[1/-1] justify-self-end lg:hidden inline-flex items-center p-2 ml-3 text-sm rounded focus:outline-none focus:ring-2 text-gray-400 hover:bg-gray-700 focus:ring-gray-600`} 
+                    aria-controls="navbar-default"
+                    aria-expanded="false"
+                    onClick={(evt) => handleClick(evt)}
+                >
+                    <svg
+                        className="w-6 h-6"
+                        aria-hidden="true"
+                        fill="currentColor"
+                        viewBox="0 0 20 20"
+                        xmlns="http://www.w3.org/2000/svg"
+                    >
+                        <path d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"></path>
+                    </svg>
+                </button>
             </div>
         </header>
     );
--- components/navigation/NavBarLinks.jsx
+++ components/navigation/NavBarLinks.jsx
@@ -4,19 +4,19 @@ function NavBarLinks({customStyles, ...props}) {
     return ( 
         <ul className={`${customStyles} lg:gap-4`}>
             <li className="w-full lg:w-[unset] text-center">
-                <Link className="w-full py-[0.25rem] lg:w-[unset] lg:py-[unset] hidden lg:inline-block" href="/">home</Link>
+                <Link className="w-full py-[0.25rem] lg:w-[unset] lg:py-[unset] inline-block" href="/">home</Link>
             </li>
             <li className="w-full lg:w-[unset] text-center">
-                <Link className="w-full py-[0.5rem] lg:w-[unset] lg:py-[unset] hidden lg:inline-block" href="/service-robots">Service Robots</Link>
+                <Link className="w-full py-[0.5rem] lg:w-[unset] lg:py-[unset] inline-block" href="/service-robots">Service Robots</Link>
             </li>
             <li className="w-full lg:w-[unset] text-center">
-                <Link className="w-full py-[0.5rem] lg:w-[unset] lg:py-[unset] hidden lg:inline-block" href="/combat-robots">Combat Robots</Link> 
+                <Link className="w-full py-[0.5rem] lg:w-[unset] lg:py-[unset] inline-block" href="/combat-robots">Combat Robots</Link> 
             </li>
             <li className="w-full lg:w-[unset] text-center">
-                <Link className="w-full py-[0.5rem] lg:w-[unset] lg:py-[unset] hidden lg:inline-block" href="/social-robots">Social Robots</Link> 
+                <Link className="w-full py-[0.5rem] lg:w-[unset] lg:py-[unset] inline-block" href="/social-robots">Social Robots</Link> 
             </li>
             <li className="w-full lg:w-[unset] text-center">
-                <Link className="w-full py-[0.5rem] lg:w-[unset] lg:py-[unset] hidden lg:inline-block" href="/cats">Cats</Link> 
+                <Link className="w-full py-[0.5rem] lg:w-[unset] lg:py-[unset] inline-block" href="/cats">Cats</Link> 
             </li>
         </ul>
      );
